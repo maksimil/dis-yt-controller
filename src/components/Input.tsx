cd3e84d94ca01bbb38ca9cbdb71fa00746c3657c
Maxim <maxim.kosterov@gmail.com>
2020-08-22 21:44:48 +0300

    Added autocomplete box

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,34 +5,67 @@ class Input extends React.Component<
     add: (s: string) => void;
     lastvalid: boolean;
   },
-  { url: string }
+  {
+    url: string;
+    focused: boolean;
+  }
 > {
   state = {
     url: "",
+    focused: false,
   };
+
+  inputref: React.RefObject<HTMLInputElement> = React.createRef();
 
   render() {
     const { add, lastvalid } = this.props;
+    const { url, focused } = this.state;
     return (
       <tr>
         <td className="text" colSpan={2}>
           <input
+            ref={this.inputref}
             className={lastvalid ? "" : "invalid"}
             type="text"
-            value={this.state.url}
+            value={url}
             placeholder="url"
+            onFocus={(e) => {
+              this.setState({ focused: true });
+            }}
+            onBlur={(e) => {
+              this.setState({ focused: false });
+            }}
             onChange={(e) => {
               this.setState({
                 url: e.target.value,
               });
             }}
           />
+          {focused ? (
+            <table
+              className="autocomplete"
+              style={{
+                width: `${
+                  this.inputref.current?.getBoundingClientRect().width
+                }px`,
+              }}
+            >
+              <tbody>
+                <tr className="autocompleteelement">
+                  <td>BOY</td>
+                </tr>
+                <tr className="autocompleteelement">
+                  <td>OH, BOY</td>
+                </tr>
+              </tbody>
+            </table>
+          ) : null}
         </td>
         <td>
           <button
             className="list"
             onClick={() => {
-              add(this.state.url);
+              add(url);
               this.setState({ url: "" });
             }}
           >

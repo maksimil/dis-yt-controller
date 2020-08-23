f574ed2e9943edec6209b461a759e251a1cd53fa
Maxim <maxim.kosterov@gmail.com>
2020-08-23 22:05:13 +0300

    implemented queue

---
+++ server/ts/bot/components/queue.ts
@@ -0,0 +1,34 @@
+class Queue<T> {
+  queue: T[];
+
+  constructor(queue?: T[]) {
+    this.queue = queue || [];
+  }
+
+  enqueue = (element: T) => {
+    this.queue.push(element);
+  };
+
+  getnext = () => this.queue[0];
+
+  shift = (loop: boolean) => {
+    const element = this.queue.shift();
+    if (!element) return undefined;
+    if (loop) this.enqueue(element);
+    return element;
+  };
+
+  removeuniqueue = (fn: (v: T) => boolean) => {
+    const index = this.queue.findIndex(fn);
+    if (index === -1) return false;
+
+    this.queue = this.queue.splice(index, 1);
+    return true;
+  };
+
+  filter = (fn: (v: T) => boolean) => {
+    this.queue = this.queue.filter(fn);
+  };
+}
+
+export default Queue;
